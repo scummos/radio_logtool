ccd703698b6adbcdce455874204ddcd487082cc9
Sven Brauch <svenbrauch@googlemail.com>
2011-09-25 13:20:57 +0200

    script working, beginning UI stuff

--- main.py
+++ main.py
@@ -1,59 +1,69 @@
-import pyaudio
+import ossaudiodev
 import wave
 import sys
 from struct import unpack, pack
 from array import array
+from time import time, sleep
+from math import floor
 
 class Recorder():
     def __init__(self):
         self.chunkSize = 1024
-        self.format = pyaudio.paInt16
+        self.format = ossaudiodev.AFMT_S16_LE
         self.channels = 1
         self.rate = 44100
-        self.p = pyaudio.PyAudio()
-        self.stream = p.open(format = self.format, channels = self.channels, rate = self.rate,
-                             input = True, frames_per_buffer = self.chunkSize)
     
-    def numFrames(self, length):
-        return length * self.rate / self.chunkSize
-    
-    def recordBlock(self, length):
+    def record(self, length):
         """Record a chunk of "length" seconds, and return it as int array"""
-        collectedData = []
-        for index in xrange(0, self.numFrames(length)):
-            try:
-                data = self.stream.read(self.chunkSize)
-            except IOError:
-                pass
-            collectedData.append(data)
-        
-        unpackedData = array('h', [])
-        for item in collectedData:
-            unpackedItem = unpack('<' + ('h'*(len(data)/2)), item)
-            unpackedData.extend(unpackedItem)
-        return unpackedData
+        print "Recording block of length", length
+        self.stream = ossaudiodev.open('r')
+        self.stream.setfmt(self.format)
+        self.stream.speed(self.rate)
+        data = self.stream.read(int(length * self.rate * 2 + 2)) # 2 = sizeof(short)
+        self.stream.close()
+        return self.unpack(data)
     
-    def recordBlocked(self, totalLength, chunkLength):
-        numChunks = totalLength / float(chunkLength)
+    def unpack(self, data):
+        return unpack('<' + ('h'*(len(data)/2)), data)
+    
+    def partition(self, data, length, integrationInterval = 1):
+        blockSize = int(self.rate * length / integrationInterval)
+        print "Partitioning data into blocks of", blockSize, "frames"
+        size = len(data)
+        atFrame = 0
         blocks = []
-        for i in range(numChunks):
-            currentBlock = recordBlock(chunkLength)
-            blocks.append(currentChunk)
+        while atFrame + blockSize <= size:
+            blocks.append(data[atFrame:atFrame+blockSize])
+            atFrame += blockSize
         return blocks
+    
+    def integrate(self, data, samples):
+        newData = array('I', [])
+        for rangeBegin in range(0, len(data), samples):
+            newData.append(sum([x**2 for x in data[rangeBegin:rangeBegin+samples]]))
+        return newData
     
     def addRegisteredBlocks(self, blocks):
         result = []
-        for frameIndex in len(blocks[0]):
+        for frameIndex in range(len(blocks[0])):
             result.append(sum([block[frameIndex] for block in blocks]))
         return result
     
     def writeArrayToDatafile(self, data, filename):
+        print "Writing datafile %s" % filename
         with open(filename, 'w') as fileptr:
             for item in data:
-                fileptr.write(item + "\n")
+                fileptr.write(str(item) + "\n")
     
 if __name__ == '__main__':
     recorder = Recorder()
-    data = recorder.recordBlocked(10, 1)
-    total = recorder.addRegisteredBlocks(data)
-    recorder.writeArrayToDatafile(total, "test.dat")
+    data = recorder.record(50)
+    integrated = recorder.integrate(data, 20)
+    partitioned = recorder.partition(integrated, 1.0, 20)
+    i = 0
+    for part in partitioned:
+        i += 1
+        recorder.writeArrayToDatafile(part, "data/%04i.dat" % i)
+    total = recorder.addRegisteredBlocks(partitioned)
+    recorder.writeArrayToDatafile(total, "sum.dat")
+    recorder.writeArrayToDatafile(data, "raw.dat")
