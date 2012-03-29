95bf312aa07f0c5acd15ba3f9ff12c87c97be183
Sven Brauch <svenbrauch@googlemail.com>
2012-03-29 02:31:04 +0200

    Nice version with integration and everything, just a few bugs left

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ import copy
 
 from datetime import datetime
 
-from PyQt4.QtGui import QMainWindow, QApplication, QColor
+from PyQt4.QtGui import QMainWindow, QApplication, QColor, QPen
 from PyQt4.QtCore import QThread, QTimer, QMutex, pyqtSignal
+from PyQt4 import QtGui, QtCore
 from PyKDE4.kio import KFile, KFileDialog
-from PyKDE4.kdeui import KPlotWidget, KPlotObject
+from PyKDE4.kdeui import KPlotWidget, KPlotObject, KMessageBox
 from PyKDE4 import kdecore
 
 from i2c import mcp3426Client as connection
@@ -49,17 +50,63 @@ def appendArrayToDatafile(data, filename):
         for item in data:
             fileptr.write(str(item) + "\n")
 
+class DelayedFileWriter():
+    def __init__(self, file, accum = 480):
+        self.data = []
+        self.accum = accum
+        self.file = file
+        
+    def write(self, data):
+        self.data.extend(data)
+        self.maybeWriteData()
+    
+    def doWriteData(self):
+        self.maybeWriteData(force = True)
+    
+    def maybeWriteData(self, force = False):
+        if len(self.data) >= self.accum or force:
+            with open(self.file, 'a') as fileptr:
+                fileptr.write('\n'.join([str(item) for item in self.data]))
+                self.data = []
+    
+    def __del__(self):
+        self.doWriteData()
+        assert self.data == []
+
 class Recorder(QThread):
     dataAvailable = pyqtSignal()
+    connectionStatusChanged = pyqtSignal(bool)
+    connectionError = pyqtSignal(str)
+    operationMode = ("16bit", "2x")
     
     def __init__(self, length):
         super(Recorder, self).__init__()
         self.dataAccessMutex = QMutex()
         self.data = []
         self.client = connection()
-        self.client.openDevice()
-        self.client.setOperationMode("16bit", "1x")
         self.length = length
+    
+    def doConnect(self):
+        try:
+            self.client.openDevice()
+            self.client.setOperationMode(*self.operationMode)
+            self.connectionStatusChanged.emit(True)
+            return True
+        except Exception as e:
+            self.connectionError.emit(str(e))
+            self.connectionStatusChanged.emit(False)
+            return False
+    
+    def reconnect(self):
+        if self.client.isInitialized():
+            try:
+                self.client.reinit()
+                self.connectionStatusChanged.emit(True)
+            except IOError as e:
+                self.connectionError.emit(str(e))
+                self.connectionStatusChanged.emit(False)
+        else:
+            self.doConnect()
     
     def cleanup(self):
         self.dataAccessMutex.lock()
@@ -77,7 +124,12 @@ class Recorder(QThread):
             #print "Recording block of length", length
             newData = []
             for i in xrange(self.length):
-                newData.append(self.client.readBlock())
+                try:
+                    newData.append(self.client.readBlock())
+                except IOError as e:
+                    self.connectionStatusChanged.emit(False)
+                    self.connectionError.emit(str(e))
+                    return
             self.dataAccessMutex.lock()
             self.data.append(newData) # 1*2 = sizeof(short)
             self.dataAccessMutex.unlock()
@@ -98,6 +150,7 @@ class mainwin(QMainWindow):
         self.ui = mainwindow.Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.record_url.setMode(KFile.Directory)
+        self.rawDataFileWriter = None
         self.ui.window_splitter.setSizes([600, 150])
         self.ui.preview_group.toggled.connect(self.toggleAutoUpdate)
         self.abortRecording = False
@@ -108,27 +161,55 @@ class mainwin(QMainWindow):
         self.dataAccessMutex = QMutex()
         self.dataQueue = False
         self.startRecording()
-        self.resultRegenerationTimer = QTimer()
         self.updatePreviewIntervals()
-        self.resultRegenerationTimer.timeout.connect(self.regenerateAndDisplayResult)
-        self.resultRegenerationTimer.start()
         self.ui.preview_repeatInterval.textChanged.connect(self.updatePreviewIntervals)
         self.ui.control_clear.clicked.connect(self.clearData)
         self.toggleDataAcquisitionAndOperationMode()
+        self.toggleAutoUpdate()
         self.chunkIndex = 0
-        self.totalPreviewGenerator = PreviewGenerator(self)
         self.data = []
         self.currentDataIndex = 0
+        self.currentIntegratedIndex = 0
         self.isFirstChunk = True
         self.ui.actionSave_displayed.activated.connect(self.saveAllData)
         self.ui.gradientSettings_integrate.textChanged.connect(self.updateIntegrationInterval)
         self.ui.record_startstop.toggled.connect(self.updateRecordingState)
         self.ui.record_url.urlSelected.connect(self.updatePath)
         self.ui.record_prefix.textChanged.connect(self.updatePath)
+        self.ui.preview_now.clicked.connect(self.redrawPlot)
+        self.ui.gradientSettings_group.clicked.connect(self.changeIntegrationMode)
+        self.ui.gradientSettings_integrate.textChanged.connect(self.changeIntegrationMode)
+        self.changeIntegrationMode()
         self.updateIntegrationInterval()
         self.updateRecordingState()
         self.updatePath()
         self.ui.plot_sum.setAntialiasing(True)
+        self.plotobject = None
+        self.integratePlotObject = None
+        self.ui.actionOpen_and_continue.activated.connect(self.openDataFile)
+        
+        self.dataAwaitingIntegration = []
+        self.invertDataToggled()
+        self.ui.invertSignalCheckbox.clicked.connect(self.invertDataToggled)
+    
+    def invertDataToggled(self):
+        self.invertData = self.ui.invertSignalCheckbox.isChecked()
+    
+    def verifyDataSaved(self):
+        if self.rawDataFileWriter is not None:
+            self.rawDataFileWriter.doWriteData()
+    
+    def changeIntegrationMode(self):
+        self.doIntegration = self.ui.gradientSettings_group.isChecked()
+        self.integrationInterval = verify(self.ui.gradientSettings_integrate.text().toInt(), 30, "integration interval", self)
+        self.recalculateIntegratedData()
+    
+    def recalculateIntegratedData(self):
+        chunkSize = self.integrationInterval
+        self.integratedData = []
+        for i in xrange(0, len(self.data), chunkSize):
+            chunk = self.data[i:i+chunkSize]
+            self.integratedData.append(sum(chunk) / chunkSize)
     
     def saveAllData(self):
         f = KFileDialog.getSaveFileName()
@@ -143,15 +224,17 @@ class mainwin(QMainWindow):
     
     def updateRecordingState(self):
         self.recordingState = self.ui.record_startstop.isChecked()
+        self.verifyDataSaved()
     
     def updateIntegrationInterval(self):
         self.integrationInterval = verify(self.ui.gradientSettings_integrate.text().toInt(), 1, "integration interval", self)
         self.integrationInterval = max([self.integrationInterval, 0.04]) # more than 25 updates per second don't make sense
     
     def updatePreviewIntervals(self):
-        self.resultRegenerationTimer.setInterval(verify(self.ui.preview_repeatInterval.text().toFloat(), 2.0, "preview interval", self) * 1000)
+        self.updateInterval = verify(self.ui.preview_repeatInterval.text().toInt(), 4, "preview interval", self)
     
     def toggleDataAcquisitionAndOperationMode(self):
+        self.verifyDataSaved()
         if not self.ui.mode_group.isChecked():
             self.abortRecording = True
         elif self.abortRecording:
@@ -169,44 +252,99 @@ class mainwin(QMainWindow):
             self.clearData()
             self.updatePreviewIntervals()
     
+    def indexToTime(self, index):
+        return index / (samplesPerSecond * 60.0)
+    
+    def addIntegratedPoints(self, points):
+        for point in points:
+            self.integratedRealIndex = self.indexToTime(self.currentIntegratedIndex) * self.integrationInterval
+            self.integratePlotObject.addPoint(self.integratedRealIndex, point)
+            self.currentIntegratedIndex += 1
+    
+    def addDataPoints(self, points):
+        for point in points:
+            self.realIndex = self.indexToTime(self.currentDataIndex)
+            self.plotobject.addPoint(self.realIndex, point)
+            self.currentDataIndex += 1
+    
+    def openDataFile(self):
+        self.verifyDataSaved()
+        f = KFileDialog.getOpenFileName()
+        s = str(f).split('/')
+        self.clearData()
+        self.dataAccessMutex.lock()
+        try:
+            with open(f) as fileptr:
+                data = fileptr.readlines()
+                self.data = [float(item) for item in data]
+            if len(self.data) == 0:
+                raise IOError("No valid data points found, probably the file is empty")
+            self.ui.record_url.setText('/'.join(s[:-1]))
+            self.ui.record_prefix.setText(s[-1].replace('result', '') + "_cont_")
+            self.ui.record_startstop.setChecked(False)
+            self.updateRecordingState()
+            self.ui.statusbar.message("Successfully loaded %s data points from %s." % (len(data), str(f)))
+        except Exception as e:
+            print e
+            try:
+                self.ui.statusbar.message("Could not load file %s: %s" % (f, str(e)))
+            except:
+                self.ui.statusbar.message("Could not load file %s: invalid characters" % f)
+        finally:
+            if len(self.data) > 0:
+                self.addDataPoints(self.data)
+            self.redrawPlot()
+            self.dataAccessMutex.unlock()
+    
     def clearData(self):
+        self.verifyDataSaved()
         self.dataAccessMutex.lock()
         self.data = []
+        self.currentDataIndex = 0
         self.previousChunk = False
+        if hasattr(self, "plotobject"):
+            self.plotobject.clearPoints()
         self.dataAccessMutex.unlock()
     
     def toggleAutoUpdate(self):
         if self.ui.preview_group.isChecked():
             self.updateRecordingInterval()
-            self.resultRegenerationTimer.start()
+            self.autoUpdate = True
         else:
-            self.resultRegenerationTimer.stop()
+            self.autoUpdate = False
     
     def length(self):
         return verify(self.ui.gradientSettings_integrate.text().toInt(), 1, "integration interval", self)
     
     def updateRecordingInterval(self):
-        self.recorderThread.length = self.length()
-        self.clearData()
+        self.recorderThread.length = 1 # TODO make configurable
     
-    def regenerateAndDisplayResult(self):
-        self.dataAccessMutex.lock()
-        data = self.data
-        self.dataAccessMutex.unlock()
-        integrated = integrateData(data, self.integrationInterval)
-        integrated.pop()
-        self.displayResult(integrated)
+    def connectionStatusChanged(self, isConnected):
+        self.deviceConnected = isConnected
+        print "Connection status changed:", isConnected
+        if isConnected:
+            text = '<font style="color:#8BBE00; font-weight:bold">connected</font>'
+            self.ui.reconnectButton.setEnabled(False)
+            self.ui.statusbar.message("i2c-tiny-usb device connected successfully.")
+            self.recorderThread.start()
+        else:
+            text = '<font style="color:#FF2600; font-weight:bold">not connected</font>'
+            self.ui.reconnectButton.setEnabled(True)
+        self.ui.connectionStatus.setText("Status: %s" % text)
     
-    def displayResult(self, data):
-        self.totalPreviewGenerator.data = data
-        self.totalPreviewGenerator.color = QColor(255, 200, 44)
-        self.totalPreviewGenerator.widget = self.ui.plot_sum
-        #self.totalPreviewGenerator.run()
+    def IOErrorOccured(self, error):
+        self.ui.statusbar.message('Connection error: %s' % error)
     
     def startRecording(self):
         self.recorderThread = Recorder(1)
+        self.ui.reconnectButton.setEnabled(True)
         self.recorderThread.dataAvailable.connect(self.handleRecordFinished)
-        self.recorderThread.start(QThread.TimeCriticalPriority)
+        self.recorderThread.connectionStatusChanged.connect(self.connectionStatusChanged)
+        self.recorderThread.connectionError.connect(self.IOErrorOccured)
+        self.recorderThread.doConnect()
+        if self.deviceConnected:
+            self.recorderThread.start(QThread.TimeCriticalPriority)
+        self.ui.reconnectButton.clicked.connect(self.recorderThread.reconnect)
     
     def targetPath(self):
         url = self.ui.record_url.url()
@@ -218,6 +356,8 @@ class mainwin(QMainWindow):
     def handleQueuedData(self):
         self.dataAccessMutex.lock()
         data = self.dataQueue
+        if self.invertData:
+            data = [-item for item in data]
         targetPath = self.cachedTargetPath
         save = self.recordingState
         if save:
@@ -232,9 +372,11 @@ class mainwin(QMainWindow):
                 print "Data seems corrupted. skipping"
                 return  
             if save:
-                appendArrayToDatafile(data, filenameForChunk)
+                if self.rawDataFileWriter is None:
+                    self.rawDataFileWriter = DelayedFileWriter(filenameForChunk)
+                self.rawDataFileWriter.write(data)
                 time = datetime.now().strftime("%H:%M:%S")
-                self.ui.statusbar.message("%s: wrote %s new values to datafile \"%s\"" % (time, len((data)), filenameForChunk))
+                self.ui.statusbar.message("%s: (delayed) wrote %s new values to datafile \"%s\"" % (time, len((data)), filenameForChunk))
         except OverflowError:
             print "!! overflow while processing data, not saving"
             data = False
@@ -242,27 +384,50 @@ class mainwin(QMainWindow):
             self.dataAccessMutex.unlock()
         if data:
             self.previousChunk = data
+            self.dataAwaitingIntegration.extend(data)
+            integratedPointsAdded = self.maybeDoIntegration()
             self.dataAccessMutex.lock()
-            if self.mode == 'gradient':
-                self.data.append(sum(narray(data)/1.0))
-            elif self.mode == 'interval':
-                self.data.append(data)
+            self.data.extend(data)
             self.dataAccessMutex.unlock()
             
-            existing = self.ui.plot_sum.plotObjects()
-            if len(existing) > 0:
-                self.plotobject = existing[0]
-            else:
-                self.plotobject = KPlotObject(QColor(255, 200, 44), KPlotObject.Lines, 1)
-            for point in data:
-                self.plotobject.addPoint(self.currentDataIndex, point)
-                self.currentDataIndex += 1
-            bounds = (0, len(self.data), min(self.data), max(self.data))
-            self.ui.plot_sum.setLimits(*bounds)
-            if len(existing) == 0:
+            created = False
+            if not self.plotobject:
+                created = True
+                self.plotobject = KPlotObject(QColor(0, 190, 255), KPlotObject.Lines, 1.0)
+            self.addDataPoints(data)
+            if created:
                 self.ui.plot_sum.addPlotObject(self.plotobject)
-            self.ui.plot_sum.update()
-                
+            
+            created = False
+            if not self.integratePlotObject:
+                created = True
+                # FIXME: the width argument for kplotobject doesn't seem to work...
+                self.integratePlotObject = KPlotObject(QColor(0, 0, 0), KPlotObject.Lines, 1.0)
+                p = QPen(QColor(200, 200, 255))
+                p.setWidth(2.5)
+                self.integratePlotObject.setLinePen(p);
+            self.addIntegratedPoints(integratedPointsAdded)
+            if created:
+                self.ui.plot_sum.addPlotObject(self.integratePlotObject)
+            
+            if (self.currentDataIndex / len(data)) % self.updateInterval == 0 and self.autoUpdate:
+                self.redrawPlot()
+            
+    
+    def maybeDoIntegration(self):
+        pointsAdded = []
+        while len(self.dataAwaitingIntegration) >= self.integrationInterval:
+            integratedValue = sum(self.dataAwaitingIntegration[:self.integrationInterval]) / self.integrationInterval
+            self.integratedData.append(integratedValue)
+            pointsAdded.append(integratedValue)
+            self.dataAwaitingIntegration = self.dataAwaitingIntegration[self.integrationInterval:]
+        return pointsAdded
+    
+    def redrawPlot(self):
+        if len(self.data) > 0:
+            bounds = (0, self.realIndex*1.05, min(self.data), max(self.data))
+            self.ui.plot_sum.setLimits(*bounds)
+        self.ui.plot_sum.update()
     
     def handleRecordFinished(self):
         if self.recorderThread.stop:
